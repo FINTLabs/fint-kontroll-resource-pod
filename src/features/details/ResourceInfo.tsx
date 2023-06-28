5e33ff52053a20f2394a32de51277b6f33ee7288
Linda Loftsgarden <linda-fintlabs@users.noreply.github.com>
2023-06-28 15:04:29 +0200

    Lagt til paper på info, og funksjonalitet på søk etter ressurs

--- src/features/details/ResourceInfo.tsx
+++ src/features/details/ResourceInfo.tsx
@@ -1,9 +1,8 @@
-import {Avatar, Box, List, ListItem, ListItemAvatar, ListItemText} from "@mui/material";
+import {Box, List, ListItem, ListItemText, Paper} from "@mui/material";
 import * as React from "react";
-import {useContext, useEffect} from "react";
+import {useContext} from "react";
 import FormGroup from '@mui/material/FormGroup';
 import Grid from '@mui/material/Grid';
-import {FlutterDash} from "@mui/icons-material";
 import {ResourceContext} from "../../context";
 
 function ResourceInfo() {
@@ -11,48 +10,50 @@ function ResourceInfo() {
     const {resourceDetails} = useContext(ResourceContext);
 
     return (
-        <Box sx={{flexGrow: 1, maxWidth: 752}}>
-            <FormGroup row>
-            </FormGroup>
-            <Grid container spacing={2}>
-                <Grid item xs={12} md={6}>
-                    <List>
-                        <ListItem>
-                            <ListItemText
-                                id={'resourceName'}
-                                primary={resourceDetails?.resourceName}
-                                secondary={'Ressurs'}
-                            />
-                        </ListItem>
-                        <ListItem>
-                            <ListItemText
-                                id={'userNameText'}
-                                 primary={resourceDetails?.resourceType}
-                                secondary={'Ressurstype'}
-                            />
-                        </ListItem>
-                        <ListItem>
-                            <ListItemText
-                                id={'orgUnitText'}
-                                 primary={resourceDetails?.applicationAccessType}
-                                secondary={'Applikasjonstilgangstype'}
-                            />
-                        </ListItem>
-                        <ListItem>
-                            <ListItemText
-                                primary={resourceDetails?.applicationAccessRole}
-                                secondary={'Tilgangsrolle'}
-                            />
-                        </ListItem>
-                        <ListItem>
-                            <ListItemText
-                                primary={resourceDetails?.accessType}
-                                secondary={'Tilgangstype'}
-                            />
-                        </ListItem>
-                    </List>
+        <Box sx={{mb: 6}}>
+            <Paper>
+                <FormGroup row>
+                </FormGroup>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} md={6}>
+                        <List>
+                            <ListItem>
+                                <ListItemText
+                                    id={'resourceName'}
+                                    primary={resourceDetails?.resourceName}
+                                    secondary={'Ressurs'}
+                                />
+                            </ListItem>
+                            <ListItem>
+                                <ListItemText
+                                    id={'userNameText'}
+                                    primary={resourceDetails?.resourceType}
+                                    secondary={'Ressurstype'}
+                                />
+                            </ListItem>
+                            <ListItem>
+                                <ListItemText
+                                    id={'orgUnitText'}
+                                    primary={resourceDetails?.applicationAccessType}
+                                    secondary={'Applikasjonstilgangstype'}
+                                />
+                            </ListItem>
+                            <ListItem>
+                                <ListItemText
+                                    primary={resourceDetails?.applicationAccessRole}
+                                    secondary={'Tilgangsrolle'}
+                                />
+                            </ListItem>
+                            <ListItem>
+                                <ListItemText
+                                    primary={resourceDetails?.accessType}
+                                    secondary={'Tilgangstype'}
+                                />
+                            </ListItem>
+                        </List>
+                    </Grid>
                 </Grid>
-            </Grid>
+            </Paper>
         </Box>
     );
 }
