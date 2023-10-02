0082dca71f6f4b2c8d9ba2ee111cf80cc809f6d0
Linda Loftsgarden <linda-fintlabs@users.noreply.github.com>
2023-10-02 12:15:57 +0200

    Endret url for users

--- src/repositories/ResourceRepository.tsx
+++ src/repositories/ResourceRepository.tsx
@@ -83,7 +83,7 @@ const getResourcePage =
 
 const getUserPage = (basePath: string, page: number, size: number,
                      userType: string, organisationUnitId: number[], searchString: string) => {
-    const baseUrl = `${basePath === '/' ? '' : basePath}/api/users/`;
+    const baseUrl = `${basePath === '/' ? '' : basePath}/api/users`;
     let queryParams = [];
 
     const sanitizedQueryString = searchString.trim();
